049240de0e3d940e2d5f8af97f06099f7b37df53
Eric Quan <equan1090@gmail.com>
2021-10-12 16:24:14 -0600

    doing categoryList

---
+++ react-app/src/components/SpecificCategory/SpecificCategory.js
@@ -0,0 +1,18 @@
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {useHistory, Link, useParams } from "react-router-dom";
+import { getProjectsThunk } from "../../store/categories";
+
+// /categories/1
+function SpecificCategory() {
+    const dispatch = useDispatch();
+    const params = useParams()
+    useEffect(() => {
+        dispatch(getProjectsThunk(params.categoryId))
+    }, [dispatch, params])
+    return(
+        null
+    )
+}
+
+export default SpecificCategory
