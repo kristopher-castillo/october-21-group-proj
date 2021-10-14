62a1300bc7ad90bd83116deb1561e6447182329c
Blair Wishom <blwishom@gmail.com>
2021-10-14 08:10:16 -0700

    working on bugs

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -5,13 +5,23 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getProjectThunk } from '../../store/project';
 
+const getFilteredProjects = (search, projects) => {
+    if (!search) {
+        return projects
+    }
+    return projects.filter(project => project.title.includes(search))
+}
+
 const SearchBar = (props) => {
     // const history = useHistory();
     const dispatch = useDispatch();
     const [searchTerm, setSearchTerm] = useState('')
     const searchState = useSelector(state => state.session.user);
 
-    const projects = useSelector(state => Object.values(state.projects?.projects?.projects));
+    const {projects} = getProjectThunk;
+    const filteredProjects = getFilteredProjects(searchTerm, projects)
+
+    // const projects = useSelector(state => Object.values(state.projects?.projects?.projects));
 
     useEffect(() => {
         (async() => {
@@ -21,14 +31,12 @@ const SearchBar = (props) => {
       }, [dispatch])
 
 
-
-
     return (
         <>
         <input
         type='search'
         placeholder={props.placeholder}
-        // onChange={(e) => () {}}
+        onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button type='submit'>submit</button></>
     )
