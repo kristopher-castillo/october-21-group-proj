50e0350127f3e96885763d7bfc66866993109c8a
Eric Quan <equan1090@gmail.com>
2021-10-16 15:00:33 -0600

    signup fixed

--- react-app/src/components/SignupFormModal/SignupForm.js
+++ react-app/src/components/SignupFormModal/SignupForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector} from "react-redux";
 // import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
-import './SignUpForm.css';
+import './SignupForm.css';
 
 function SignupForm() {
   const dispatch = useDispatch();
