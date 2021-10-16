437c207ab8eb20c0ebeeec9d379eafbab613f922
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-10-16 18:39:36 -0400

    Made projects links on home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Redirect, useHistory, useParams } from "react-router-dom";
+import { Redirect, useHistory, Link } from "react-router-dom";
 import { deleteProjectThunk, getSpecificProjectThunk, projectAmountThunk, getProjectThunk } from "../../store/project";
 import './HomePage.css'
 
@@ -39,39 +39,45 @@ const HomePage = () => {
       <div className="projects-container">
         <div className="featured-projects">
           <p id="featured-projects-title">FEATURED PROJECT</p>
-          <div className="featured-projects-image">
-            <img
-              id="featured-projects-image"
-              alt={"featured proj"}
-              src={featuredProject?.image_url}
-            ></img>
-          </div>
-          <div className="featured-projects-info">
-            <p id="selected-featured-title">{featuredProject?.title}</p>
-            <p id="selected-featured-description">
-              {featuredProject?.descrption}
-            </p>
-          </div>
+          <Link to={`/projects/${featuredProject?.id}`}>
+            <div className="featured-projects-item">
+              <div className="featured-projects-image">
+                <img
+                  id="featured-projects-image"
+                  alt={"featured proj"}
+                  src={featuredProject?.image_url}
+                ></img>
+              </div>
+              <div className="featured-projects-info">
+                <p id="selected-featured-title">{featuredProject?.title}</p>
+                <p id="selected-featured-description">
+                  {featuredProject?.descrption}
+                </p>
+              </div>
+            </div>
+          </Link>
         </div>
 
         <div className="recommended-projects">
           <p id="recommended-projects-title">MORE FOR YOU</p>
           {recommendedProjects?.map(project => (
-            <li className='recommended-projects-item'>
-              <div className="recommended-projects-image">
-                <img
-                  id="recommended-projects-image"
-                  alt={"recommended proj"}
-                  src={project.image_url}
-                ></img>
-              </div>
-              <div className="recommended-projects-info">
-                <p id="selected-recommended-title">{project.title}</p>
-                <p id="selected-featured-description">
-                  {project.description}
-                </p>
-              </div>              
-            </li>
+            <Link to={`/projects/${project.id}`} key={project.id}>
+              <li className='recommended-projects-item' >
+                <div className="recommended-projects-image">
+                  <img
+                    id="recommended-projects-image"
+                    alt={"recommended proj"}
+                    src={project.image_url}
+                  ></img>
+                </div>
+                <div className="recommended-projects-info">
+                  <p id="selected-recommended-title">{project.title}</p>
+                  <p id="selected-featured-description">
+                    {project.description}
+                  </p>
+                </div>              
+              </li>
+            </Link>
           ))}
         </div>
       </div>
