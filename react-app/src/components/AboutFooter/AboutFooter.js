2f3d2fd5592bf32a911378c3d017c92df457e2db
Kristopher C. Castillo <kccastillo2@gmail.com>
2021-11-03 17:38:27 -0400

    Updated meta tags in index.html.

--- react-app/src/components/AboutFooter/AboutFooter.js
+++ react-app/src/components/AboutFooter/AboutFooter.js
@@ -8,7 +8,7 @@ const AboutFooter = () => {
         <div className="footer">
             <div className="follow">
             <p>Follow Us</p>
-            <img src="../GitHub-Mark-32px.png" alt="" className="github-logo"></img>
+            {/* <img src="../GitHub-Mark-32px.png" alt="" className="github-logo"></img> */}
             </div>
             <div className="footer-members">
                 <div className="footer-item">
