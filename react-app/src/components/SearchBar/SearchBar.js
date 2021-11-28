f0f2914384a5adf992907e3bd4bd093fe9d18f21
Kristopher C. Castillo <kccastillo2@gmail.com>
2021-11-27 19:27:17 -0500

    Search bar results dissappear on loss of focus.

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { getProjectsSearchThunk } from '../../store/search';
 import './SearchBar.css'
 
@@ -8,6 +8,7 @@ const SearchBar = (props) => {
     const dispatch = useDispatch();
     const projects = useSelector(state => state.search?.projects);
     const [searchTerm, setSearchTerm] = useState('');
+    const [searchBarHidden, setSearchBarHidden] = useState(false)
     const history = useHistory();
 
 
@@ -26,24 +27,41 @@ const SearchBar = (props) => {
     const result = getFilteredProjects(searchTerm, projects)
 
     return (
-        <>
-        <div className="search-bar-div">
-            <input
-            type='text'
-            placeholder='Search'
-            onChange={(e) => setSearchTerm(e.target.value)}
+      <>
+        <div
+          className="search-bar-div"
+          tabIndex="0"
+          onBlur={(e) => {
+              if (!e.currentTarget.contains(e.relatedTarget)) {
+                setSearchBarHidden(true);
+            }
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+            onClick={() => setSearchBarHidden(false)}
             value={searchTerm}
             className="search-bar-input"
-            />
-        {/* <button type='submit' onClick={() => history.push(`/projects/${result?.id}`)}>submit</button> */}
-                <ul className="search-dropdown">
-                {result?.map((project) => <li onClick={() =>
-                    {setSearchTerm('')
-                    history.push(`/projects/${project?.id}`)}} key={project.id} className={`results-${project.id}`}>{project?.title}</li>)}
-             </ul>
+          />
+          <ul className="search-dropdown" hidden={searchBarHidden}>
+            {result?.map((project) => (
+              <Link
+                to={`/projects/${project?.id}`}
+                onClick={() => setSearchTerm("")}
+                key={project.id}
+                className={`results-${project.id}`}
+              >
+                {project?.title}
+              </Link>
+            ))}
+          </ul>
         </div>
-        </>
-    )
+      </>
+    );
 }
 
 export default SearchBar
